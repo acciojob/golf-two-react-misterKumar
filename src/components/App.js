f474f328f6c2bdcd68c944d8d41f55e01e2390c3
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-16 19:59:07 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,77 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({
+      left: "0px",
+      top: "0px",
+    });
+  };
+
+  const handleArrowKeys = (event) => {
+    let updatedX = x;
+    let updatedY = y;
+
+    switch (event.key) {
+      case "ArrowRight":
+        updatedX += 5;
+        break;
+      case "ArrowLeft":
+        updatedX -= 5;
+        break;
+      case "ArrowUp":
+        updatedY -= 5;
+        break;
+      case "ArrowDown":
+        updatedY += 5;
+        break;
+      default:
+        break;
+    }
+
+    setX(updatedX);
+    setY(updatedY);
+    setBallPosition({
+      left: `${updatedX}px`,
+      top: `${updatedY}px`,
+    });
+  };
+
+  useEffect(() => {
+    if (renderBall) {
+      window.addEventListener("keydown", handleArrowKeys);
+    } else {
+      window.removeEventListener("keydown", handleArrowKeys);
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleArrowKeys);
+    };
+  }, [renderBall, x, y]);
+
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else {
+      return (
+        <button onClick={() => setRenderBall(true)} className="start">
+          Start
+        </button>
+      );
+    }
+  };
 
   return (
     <div className="playground">
